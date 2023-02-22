7f24a51407d716f3e3c0afbbae59680c32b54b40
Rickzerr94 <rickzerr91@gmail.com>
2023-02-22 20:38:58 +0200

    add slick slider + adaptive

--- js/script.js
+++ js/script.js
@@ -7,3 +7,37 @@ function documentClick(e) {
         document.documentElement.classList.toggle('menu-open');
     }
 };
+
+const settingSlider = {
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    arrows: false,
+    mobileFirst: false,
+    responsive: [
+        {
+            breakpoint: 860,
+            settings: {
+                slidesToShow: 4,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 390,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        },
+    ]
+}
+
+$(document).ready(function (){
+    $('.clients__slider').slick(settingSlider);
+})
